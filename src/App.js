9f3c48f1d5148591d28f3a9244d920e80d094aca
ZiYenChang <you@example.com>
2021-06-29 03:31:20 +0100

    update my react app to github pages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     <>
       <Router>
           <Header/>
-          <Article1/>
         <Switch>
           <Route exact path='/' component={Article1}/>
           <Route path='/article2' component={Article2}/>
